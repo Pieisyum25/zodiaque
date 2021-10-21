75805cbf4b66946096966d6e6fad1ca089f742f6
John Flynn <johnhfflynn25@gmail.com>
2021-10-21 15:43:40 +1300

    Set up navigation component

--- app/src/main/java/com/jumpstopstudios/zodiaque/SignDetailFragment.kt
+++ app/src/main/java/com/jumpstopstudios/zodiaque/SignDetailFragment.kt
@@ -1,4 +1,33 @@
 package com.jumpstopstudios.zodiaque
 
-class SignDetailFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jumpstopstudios.zodiaque.databinding.FragmentSignDetailBinding
+
+class SignDetailFragment : Fragment() {
+
+    private var _binding: FragmentSignDetailBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSignDetailBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/jumpstopstudios/zodiaque/SignItemFragment.kt
+++ app/src/main/java/com/jumpstopstudios/zodiaque/SignItemFragment.kt
@@ -1,4 +1,33 @@
 package com.jumpstopstudios.zodiaque
 
-class SignItemFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.jumpstopstudios.zodiaque.databinding.FragmentSignItemBinding
+
+class SignItemFragment : Fragment() {
+
+    private var _binding: FragmentSignItemBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSignItemBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
